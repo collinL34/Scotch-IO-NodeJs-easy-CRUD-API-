d71f73bc23f0e9fabcedbecaaa07787c4045b2ca
collinl34 <collinlshelby@gmail.com>
2017-09-11 15:18:16 -0700

    update route error fix along with minimal refactoring of code

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(bodyParser.urlencoded({ extend: false }));
 app.use(bodyParser.json());
 
 app.listen(3000, () => {
-    console.log('App Successful listening on port 3000');
+    console.log('listening on port 3000');
 });
 
 app.get('/cars', (req, res) => {
     Car.find((err, cars) => {
         if (err)
-            console.log('Error Error you fukced up!!');
+            console.log(handleError(err));
         res.json(cars);
     });
 });
@@ -28,39 +28,39 @@ app.post('/cars', (req, res) => {
         year: req.query.year
     }, (err, cars) => {
         if (err)
-            console.log('Error');
+            console.log(handleError(err));
         Car.find((err, cars) => {
             if (err)
-                console.log('Error');
-            res.json(cars);
-        });
-    })
-});
-
-app.put('/cars/:id', (req, res) => {
-    Car.update(req.params.id, req.query, (err, cars) => {
-        if (err)
-            console.log(err);
-        Car.find((err, cars) => {
-            if (err)
-                console.log(err);
+                console.log(handleError(err));
             res.json(cars);
         });
     });
-})
+});
+
+app.put('/cars/:id', (req, res) => {
+    Car.findById(req.params.id, (err, car) => {
+        car.update(req.query, (err, cars) => {
+            if (err)
+                console.log(handleError(err));
+            Car.find((err, cars) => {
+                if (err)
+                    console.log(handleError(err));
+                res.json(cars);
+            });
+        });
+    });
+});
 
 app.delete('/cars/:id', (req, res) => {
     Car.remove({
         _id: req.params.id
     }, (err, cars) => {
         if (err)
-            console.log('Error');
+            console.log(handleError(err));
         Car.find((err, cars) => {
             if (err)
-                console.log('Error');
+                console.log(handleError(err));
             res.json(cars);
         });
     });
 });
-
-
